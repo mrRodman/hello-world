b3b46abbdcecf15e7acaa2c3dbcc2ce8c0d4c859
mrRodman <orix.pesh.com@gmail.com>
2018-01-19 20:06:15 +0200

    Add files via upload

---
+++ photos/src/main/java/ua/kiev/prog/PhotoErrorException.java
@@ -0,0 +1,14 @@
+package ua.kiev.prog;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class PhotoErrorException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
